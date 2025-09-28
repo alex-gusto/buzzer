61597636a34ae8c1ece1142e24358f56c567ead0
Oleksandr Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2025-09-28 10:15:27 +0200

    ui fix

--- apps/client/src/components/host/HostTurnControls.tsx
+++ apps/client/src/components/host/HostTurnControls.tsx
@@ -103,7 +103,7 @@ export function HostTurnControls({
         </div>
       </div>
 
-      <div className="flex flex-wrap gap-2">
+      <div className="flex gap-2">
         <button
           type="button"
           onClick={onActivate}
